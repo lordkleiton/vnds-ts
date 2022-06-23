893bfdd278507f6b6a215284377a905310e76056
lordkleiton <m_s_r@outlook.com>
2022-06-23 14:06:24 -0300

    testando set do arquivo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import { ScriptEngine } from "~/classes";
+import { IVNDS } from "~/interfaces";
 
+const button = document.querySelector("#botao") as HTMLButtonElement;
+
+button.onclick = () => {
+  window.showOpenFilePicker().then(async files => {
+    const vnds = {} as IVNDS;
+    const engine = new ScriptEngine(vnds);
+    const file_handle = files[0];
+    const file = await file_handle.getFile();
+
+    engine.setScriptFile(file);
+  });
+};
