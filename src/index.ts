ea55826407772679eacaaa996e09a39b9f383e86
lordkleiton <m_s_r@outlook.com>
2022-06-23 14:50:45 -0300

    liberando objeto no window pra observar mais facilmente

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { IVNDS } from "~/interfaces";
 
 const button = document.querySelector("#botao") as HTMLButtonElement;
 
+const vnds = {} as IVNDS;
+const engine = new ScriptEngine(vnds);
+
+Object.assign(window, { engine });
+
 button.onclick = () => {
   window.showOpenFilePicker().then(async files => {
-    const vnds = {} as IVNDS;
-    const engine = new ScriptEngine(vnds);
     const file_handle = files[0];
     const file = await file_handle.getFile();
 
