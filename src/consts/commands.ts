35b0e5de65c918c244cdc0e978726696c0361c42
lordkleiton <m_s_r@outlook.com>
2022-06-24 15:31:02 -0300

    comandos disponiveis

---
+++ src/consts/commands.ts
@@ -0,0 +1,33 @@
+export const COMMAND_BGLOAD = "bgload ";
+
+export const COMMAND_SETIMG = "setimg ";
+
+export const COMMAND_SOUND = "sound ";
+
+export const COMMAND_MUSIC = "music ";
+
+export const COMMAND_TEXT = "text ";
+
+export const COMMAND_CHOICE = "choice ";
+
+export const COMMAND_SETVAR = "setvar ";
+
+export const COMMAND_GSETVAR = "gsetvar ";
+
+export const COMMAND_IF = "if ";
+
+export const COMMAND_FI = "fi ";
+
+export const COMMAND_JUMP = "jump ";
+
+export const COMMAND_DELAY = "delay ";
+
+export const COMMAND_RANDOM = "random ";
+
+export const COMMAND_LABEL = "label ";
+
+export const COMMAND_GOTO = "goto ";
+
+export const COMMAND_CLEARTEXT = "cleartext ";
+
+export const COMMAND_ENDSCRIPT = "endscript ";
