d788de753a1be599605875da6d691a806d7aad75
lord <m_s_r@outlook.com>
2022-06-25 09:06:04 -0300

    portando execute

--- src/classes/script_interpreter.ts
+++ src/classes/script_interpreter.ts
@@ -1,3 +1,4 @@
+import { CommandType } from "~enums";
 import { ICommand, IScriptInterpreter, IVNDS } from "~interfaces";
 
 export default class ScriptInterpreter implements IScriptInterpreter {
@@ -50,7 +51,66 @@ export default class ScriptInterpreter implements IScriptInterpreter {
   cmdText(cmd: ICommand, quickread: boolean, skipread: boolean): void {
     throw new Error("Method not implemented.");
   }
+
   execute(cmd: ICommand, quickread: boolean): void {
-    throw new Error("Method not implemented.");
+    switch (cmd.id) {
+      case CommandType.TEXT:
+        this.cmdText(cmd, quickread, false);
+        return;
+      case CommandType.SETIMG:
+        this._cmd_setimg(cmd, quickread);
+        return;
+      case CommandType.BGLOAD:
+        this._cmd_bgload(cmd, quickread);
+        return;
+      case CommandType.SOUND:
+        this._cmd_sound(cmd, quickread);
+        return;
+      case CommandType.MUSIC:
+        this._cmd_music(cmd, quickread);
+        return;
+      case CommandType.SKIP:
+        this._cmd_skip(cmd, quickread);
+        return;
+      case CommandType.CHOICE:
+        this._cmd_choice(cmd, quickread);
+        return;
+      case CommandType.SETVAR:
+        this._cmd_setvar(cmd, quickread);
+        return;
+      case CommandType.GSETVAR:
+        this._cmd_gsetvar(cmd, quickread);
+        return;
+      case CommandType.IF:
+        this._cmd_if(cmd, quickread);
+        return;
+      case CommandType.FI:
+        this._cmd_fi(cmd, quickread);
+        return;
+      case CommandType.JUMP:
+        this._cmd_jump(cmd, quickread);
+        return;
+      case CommandType.DELAY:
+        this._cmd_delay(cmd, quickread);
+        return;
+      case CommandType.RANDOM:
+        this._cmd_random(cmd, quickread);
+        return;
+      case CommandType.LABEL:
+        this._cmd_label(cmd, quickread);
+        return;
+      case CommandType.GOTO:
+        this._cmd_goto(cmd, quickread);
+        return;
+      case CommandType.CLEARTEXT:
+        this._cmd_cleartext(cmd, quickread);
+        return;
+      case CommandType.ENDSCRIPT:
+        this._cmd_endscript(cmd, quickread);
+        return;
+      case CommandType.END_OF_FILE:
+        this._cmd_eof(cmd, quickread);
+        return;
+    }
   }
 }
