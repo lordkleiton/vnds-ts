d99aa702d815f4a17895f69806a854f0885fbf20
lord <m_s_r@outlook.com>
2022-06-25 12:35:36 -0300

    interface graphicsengine

--- src/interfaces/graphics_engine.ts
+++ src/interfaces/graphics_engine.ts
@@ -1,1 +1,13 @@
-export default interface IGraphicsEngine {}
+export default interface IGraphicsEngine {
+  isImageCached(path: string): boolean;
+  getBackgroundPath(): string;
+  getSprites(): any;
+  getNumberOfSprites(): number;
+
+  reset(): void;
+  flush(quickread: boolean): void;
+  clearCache(): void;
+  isBackgroundChanged(): boolean;
+  setBackground(filename: string, fadeTime: number): void;
+  setForeground(filename: string, x: number, y: number): void;
+}
