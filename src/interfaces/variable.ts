ad834d46d74b9d0ea3db7cf46575e681b15bf1a9
lord <m_s_r@outlook.com>
2022-06-25 10:58:16 -0300

    operações de variaveis

--- src/interfaces/variable.ts
+++ src/interfaces/variable.ts
@@ -4,4 +4,11 @@ export default interface IVariable {
   type: VarType;
   intval: number;
   strval: string;
+
+  equal(other: IVariable): boolean;
+  diff(other: IVariable): boolean;
+  gte(other: IVariable): boolean;
+  lte(other: IVariable): boolean;
+  gt(other: IVariable): boolean;
+  lt(other: IVariable): boolean;
 }
