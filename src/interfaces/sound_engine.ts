a2a4fb6de55e3fa6eece4f20fccb64ee25d472cf
lord <m_s_r@outlook.com>
2022-06-25 17:19:29 -0300

    ajustando tipagem

--- src/interfaces/sound_engine.ts
+++ src/interfaces/sound_engine.ts
@@ -3,7 +3,7 @@ export default interface ISoundEngine {
   update(): void;
   setMusic(path: string): void;
   stopMusic(): void;
-  playSound(path: string, times: number): void;
+  playSound(path: string, times?: number): void;
   stopSound(): void;
   replaySound(): void;
   setMuted(m: boolean): void;
