dbf2f4c7eac1c932e48829ae23fe61bb43214500
lord <m_s_r@outlook.com>
2022-06-25 12:38:08 -0300

    interface soundengine

--- src/interfaces/sound_engine.ts
+++ src/interfaces/sound_engine.ts
@@ -1,1 +1,16 @@
-export default interface ISoundEngine {}
+export default interface ISoundEngine {
+  reset(): void;
+  update(): void;
+  setMusic(path: string): void;
+  stopMusic(): void;
+  playSound(path: string, times: number): void;
+  stopSound(): void;
+  replaySound(): void;
+  setMuted(m: boolean): void;
+
+  isMuted(): boolean;
+  getMusicPath(): string;
+
+  setSoundVolume(v: number): void;
+  setMusicVolume(v: number): void;
+}
