674d6d78ae31815fa64ef6c9eded1cc60bfc6276
lordkleiton <m_s_r@outlook.com>
2022-06-27 14:31:38 -0300

    mockando implementação

---
+++ src/classes/sound_engine.ts
@@ -0,0 +1,74 @@
+import { ISoundEngine } from "~interfaces";
+
+export default class SoundEngine implements ISoundEngine {
+  reset(): void {
+    return;
+    throw new Error("Method not implemented.");
+  }
+
+  update(): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  setMusic(path: string): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  stopMusic(): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  playSound(path: string, times?: number | undefined): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  stopSound(): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  replaySound(): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  setMuted(m: boolean): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  isMuted(): boolean {
+    return false;
+
+    throw new Error("Method not implemented.");
+  }
+
+  getMusicPath(): string {
+    return "";
+
+    throw new Error("Method not implemented.");
+  }
+
+  setSoundVolume(v: number): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+
+  setMusicVolume(v: number): void {
+    return;
+
+    throw new Error("Method not implemented.");
+  }
+}
