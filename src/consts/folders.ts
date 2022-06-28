6c3816ef6f4e1313b7ac0f27803f91614cd60bd4
lordkleiton <m_s_r@outlook.com>
2022-06-28 10:08:28 -0300

    nome das pastas nao deve conter /

--- src/consts/folders.ts
+++ src/consts/folders.ts
@@ -1,7 +1,7 @@
-export const FOLDER_SCRIPTS = "scripts/";
+export const FOLDER_SCRIPTS = "scripts";
 
-export const FOLDER_SOUND = "sound/";
+export const FOLDER_SOUND = "sound";
 
-export const FOLDER_FG = "foreground/";
+export const FOLDER_FG = "foreground";
 
-export const FOLDER_BG = "background/";
+export const FOLDER_BG = "background";
