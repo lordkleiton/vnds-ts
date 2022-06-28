15431e3091dab65b244a2ffccee3a7bb6d46cc99
lordkleiton <m_s_r@outlook.com>
2022-06-28 11:35:39 -0300

    carregando imagens no canvas (2d)

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { INovelInfo } from "~/interfaces";
 import VNDS from "~/classes/vnds";
 import Logger from "~/classes/logger";
 import { FILE_MAIN } from "~/consts/files";
+import { ZipReaderUtils } from "~/utils";
+import * as zip from "@zip.js/zip.js";
 
 const file_name = FILE_MAIN;
 
 const button = document.querySelector("#botao") as HTMLButtonElement;
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
 const vnds = new VNDS({} as INovelInfo);
 
@@ -18,12 +21,40 @@ button.onclick = async () => {
 
   vnds.root_folder = dir_handle;
 
-  try {
-    const file_handle = await dir_handle.getFileHandle(file_name);
-    const file = await file_handle.getFile();
+  const bg = await dir_handle.getFileHandle("background.zip");
 
-    await engine.setScriptFile(file);
-  } catch (e) {
-    Logger.error(`Cannot find '${file_name}':`, e);
-  }
+  const read = ZipReaderUtils.readZippedFile(await bg.getFile());
+  const entries = await read.getEntries();
+
+  entries.forEach(async e => {
+    if (e.filename == "background/genshi.jpg") {
+      const aa = await ZipReaderUtils.getFile(e);
+
+      if (aa) {
+        const url = window.URL;
+        const ctx = canvas.getContext("2d");
+
+        if (ctx) {
+          const img = new Image();
+
+          console.log(url.createObjectURL(aa));
+
+          img.src = url.createObjectURL(aa);
+
+          img.onload = () => {
+            ctx.drawImage(img, 0, 0, 500, 300);
+          };
+        }
+      }
+    }
+  });
+
+  // try {
+  //   const file_handle = await dir_handle.getFileHandle(file_name);
+  //   const file = await file_handle.getFile();
+
+  //   await engine.setScriptFile(file);
+  // } catch (e) {
+  //   Logger.error(`Cannot find '${file_name}':`, e);
+  // }
 };
