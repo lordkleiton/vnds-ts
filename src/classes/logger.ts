a771f84d2d597a1595f0f986491f482b928ef08b
lordkleiton <m_s_r@outlook.com>
2022-06-28 09:09:14 -0300

    ajustando logger pra ter a mesma assinatura do console

--- src/classes/logger.ts
+++ src/classes/logger.ts
@@ -3,19 +3,23 @@ type ConsoleMethods = "log" | "warn" | "error";
 export default abstract class Logger {
   static enabled = true;
 
-  private static _handle(method: ConsoleMethods, message: any) {
-    if (this.enabled) console[method](message);
+  private static _handle(
+    method: ConsoleMethods,
+    message?: any,
+    ...optionalParams: any[]
+  ) {
+    if (this.enabled) console[method](message, optionalParams);
   }
 
-  static log(message: any): void {
-    this._handle("log", message);
+  static log(message?: any, ...optionalParams: any[]): void {
+    this._handle("log", message, optionalParams);
   }
 
-  static warn(message: any): void {
-    this._handle("warn", message);
+  static warn(message?: any, ...optionalParams: any[]): void {
+    this._handle("warn", message, optionalParams);
   }
 
-  static error(message: any): void {
-    this._handle("error", message);
+  static error(message?: any, ...optionalParams: any[]): void {
+    this._handle("error", message, optionalParams);
   }
 }
