cd13d3848049e3643c092ba7b2ca81adce19d28f
lordkleiton <m_s_r@outlook.com>
2022-06-28 15:28:13 -0300

    nova função de carregar script inciial

--- src/shared/utils/file_reader.ts
+++ src/shared/utils/file_reader.ts
@@ -1,3 +1,11 @@
+import {
+  FILE_MAIN,
+  FILE_SCRIPT,
+  FILE_SCRIPT_MAIN,
+  FOLDER_SCRIPT,
+} from "~/shared/consts";
+import ZipReaderUtils from "./zip_reader";
+
 export default abstract class FileReaderUtils {
   static async read(
     file: File,
@@ -12,5 +20,50 @@ export default abstract class FileReaderUtils {
 
   static async readFullFile(file: File): Promise<ArrayBuffer> {
     return this.read(file);
+  }
+
+  private static async _getScriptZip(
+    dir_handle: FileSystemDirectoryHandle
+  ): Promise<File | undefined> {
+    try {
+      const file_handle = await dir_handle.getFileHandle(FILE_SCRIPT);
+      const file = await file_handle.getFile();
+
+      const read = ZipReaderUtils.readZippedFile(file);
+      const entries = await read.getEntries();
+      const entry = entries.find(e => e.filename == FILE_SCRIPT_MAIN);
+
+      if (entry) {
+        return await ZipReaderUtils.getFile(entry);
+      }
+    } catch {
+      return;
+    }
+  }
+
+  private static async _getScriptFolder(
+    dir_handle: FileSystemDirectoryHandle
+  ): Promise<File | undefined> {
+    try {
+      const script_handler = await dir_handle.getDirectoryHandle(FOLDER_SCRIPT);
+      const file_handle = await script_handler.getFileHandle(FILE_MAIN);
+      const file = file_handle.getFile();
+
+      return file;
+    } catch {
+      return;
+    }
+  }
+
+  static async getInitialScript(
+    dir_handle: FileSystemDirectoryHandle
+  ): Promise<File | undefined> {
+    const zip = await this._getScriptZip(dir_handle);
+
+    if (!zip) {
+      return await this._getScriptFolder(dir_handle);
+    }
+
+    return zip;
   }
 }
