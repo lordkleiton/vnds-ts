1f572a3a3dbb33235aad291cb777cff9bbc6db95
lordkleiton <m_s_r@outlook.com>
2022-06-28 09:56:49 -0300

    loading folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ScriptEngine } from "~/classes";
 import { INovelInfo } from "~/interfaces";
 import VNDS from "~/classes/vnds";
+import Logger from "~/classes/logger";
 
 const button = document.querySelector("#botao") as HTMLButtonElement;
 
@@ -8,12 +9,16 @@ const vnds = new VNDS({} as INovelInfo);
 
 Object.assign(window, { engine: vnds.scriptEngine });
 
-button.onclick = () => {
-  window.showOpenFilePicker().then(async files => {
-    const engine: ScriptEngine = vnds.scriptEngine as ScriptEngine;
-    const file_handle = files[0];
+button.onclick = async () => {
+  const dir_handle = await window.showDirectoryPicker();
+  const engine: ScriptEngine = vnds.scriptEngine as ScriptEngine;
+
+  try {
+    const file_handle = await dir_handle.getFileHandle("main.scr");
     const file = await file_handle.getFile();
 
     await engine.setScriptFile(file);
-  });
+  } catch (e) {
+    Logger.error("Cannot find 'main.scr':", e);
+  }
 };
