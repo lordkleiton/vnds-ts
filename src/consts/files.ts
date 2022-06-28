be55c39347ea955d758741e14abf5560630fda9e
lordkleiton <m_s_r@outlook.com>
2022-06-28 10:22:37 -0300

    consts de nome de arquivos importantes

---
+++ src/consts/files.ts
@@ -0,0 +1,9 @@
+export const FILE_MAIN = "main.scr";
+
+export const FILE_SCRIPT = "script.zip";
+
+export const FILE_SOUND = "sound.zip";
+
+export const FILE_FG = "foreground.zip";
+
+export const FILE_BG = "background.zip";
