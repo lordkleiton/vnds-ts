6255d05130f05f7c00a27e89416cc75644234c4e
lordkleiton <m_s_r@outlook.com>
2022-06-29 14:18:49 -0300

    trocando nomes

--- src/shared/consts/elements.ts
+++ src/shared/consts/elements.ts
@@ -1,5 +1,5 @@
-export const ELEMENT_TEXT_HISTORY = "#history-text-area";
+export const ELEMENT_TEXT_HISTORY = ".history-text-area";
 
-export const ELEMENT_TEXT_CURRENT = "#current-text-area";
+export const ELEMENT_TEXT_CURRENT = ".current-text-area";
 
-export const ELEMENT_CANVAS = "#canvas";
+export const ELEMENT_CANVAS = ".canvas";
