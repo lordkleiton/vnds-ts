7c2e0c33b0466c04e7ef7173592af87ec72d584f
lordkleiton <m_s_r@outlook.com>
2022-06-29 14:48:04 -0300

    const play area

--- src/shared/consts/elements.ts
+++ src/shared/consts/elements.ts
@@ -3,3 +3,5 @@ export const ELEMENT_TEXT_HISTORY = ".history-text-area";
 export const ELEMENT_TEXT_CURRENT = ".current-text-area";
 
 export const ELEMENT_CANVAS = ".canvas";
+
+export const ELEMENT_PLAY_AREA = "#play-area";
