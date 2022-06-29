01d470113b624f415ac6231219cb7b64389b0683
lordkleiton <m_s_r@outlook.com>
2022-06-29 15:36:22 -0300

    novo elemento

--- src/shared/consts/elements.ts
+++ src/shared/consts/elements.ts
@@ -5,3 +5,5 @@ export const ELEMENT_TEXT_CURRENT = ".current-text-area";
 export const ELEMENT_CANVAS = ".canvas";
 
 export const ELEMENT_PLAY_AREA = "#play-area";
+
+export const ELEMENT_TEXT_AREA = ".text-area";
