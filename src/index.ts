94d7d151615465f9a7383b59bb4c72a0d9649a16
lordkleiton <m_s_r@outlook.com>
2022-06-30 17:07:03 -0300

    alterando besterinhas visuais

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import VNDS from "~/core/classes/vnds";
 import { Logger } from "~/app/other";
 import { ScriptEngine } from "~/app/engines";
 import { DomUtils, FileReaderUtils, StringUtils } from "~/shared/utils";
+import { CC_NEW_LINE, SCRIPT_READ_BUFFER_SIZE } from "~/shared/consts";
 
 const button = document.querySelector("#botao") as HTMLButtonElement;
 
@@ -16,11 +17,55 @@ button.onclick = async () => {
 
   vnds.root_folder = dir_handle;
 
+  // título
+
+  try {
+    const file = await FileReaderUtils.getInfoFile(dir_handle);
+
+    if (file) {
+      const read = await FileReaderUtils.read(file, 0, SCRIPT_READ_BUFFER_SIZE);
+      const buffer = new Uint8Array(read);
+
+      const last_line_index = Array.from(buffer)
+        .reverse()
+        .findIndex(el => el == CC_NEW_LINE);
+      const current_text = buffer.slice(0, buffer.length - last_line_index);
+
+      const decoder = new TextDecoder();
+      const decoded = decoder.decode(current_text);
+      const split = decoded
+        .split(RegExp("title=", "gi"))
+        .map(command => command.trim());
+
+      const title = split[1];
+
+      if (title) {
+        document.title = `${document.title}: ${title}`;
+      }
+    }
+  } catch {}
+
+  // favicon
+
+  try {
+    const file = await FileReaderUtils.getFavicon(dir_handle);
+
+    if (file) {
+      const url = window.URL.createObjectURL(file);
+
+      DomUtils.changeFavicon(url);
+    }
+  } catch {}
+
+  // fonte
+
   try {
     const file = await FileReaderUtils.getFont(dir_handle);
 
     if (file) DomUtils.setFont(file);
   } catch {}
+
+  // script
 
   try {
     const file = await FileReaderUtils.getInitialScript(dir_handle);
